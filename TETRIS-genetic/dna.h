02f1a9bbcee0d5009b260c1fb67466e4bc7bf8cb
dldyou <60469410+dldyou@users.noreply.github.com>
2022-08-02 10:49:27 +0900

    Update dna.h

--- TETRIS-genetic/dna.h
+++ TETRIS-genetic/dna.h
@@ -4,19 +4,37 @@
 
 struct DNAW {
 	int line = 0;
+	int block = 0;
 	double weight[8];
 };
 
+int GetC(void);
+int GetIdx(void);
+int GetIdxB(void);
+int GetG(void);
+int GetL(void);
+int GetB(void);
 double GetW(int idx);
+double GetWT(int parent, int idx);
+void AddScore(int parent, int score);
+void AddBlock(int parent);
+void NextGeneration(void);
+int GetP(void);
+int GetLT(int parent);
+int GetBT(int parent);
 
 class DNA {
 public:
 	void GenerateNew(void);
-	void best(void);
+	int LoadFile(void);
+	void SaveFile(void);
 	DNAW CrossOver(int parent1, int parent2);
+	DNAW CrossOver2(int parent1, int parent2);
 	DNAW Recombination(int parent1, int parent2);
 	DNAW MutationTransform(int parent);
-	DNAW MutationExchange(int parent);
+	DNAW MutationLittle(int parent);
+	DNAW MutationReverse(int parent);
+	DNAW MutationNew(int parent);
 private:
 
 };

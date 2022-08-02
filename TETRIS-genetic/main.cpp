21420d97f4b8cd8f4b76741db576131c578fb142
dldyou <60469410+dldyou@users.noreply.github.com>
2022-08-02 10:47:16 +0900

    Update main.cpp

--- TETRIS-genetic/main.cpp
+++ TETRIS-genetic/main.cpp
@@ -1,37 +1,64 @@
 #include <stdlib.h>
+#include <Windows.h>
 #include <time.h>
+#include <thread>
+#include <conio.h>
 #include "stage.h"
 #include "key.h"
 #include "training.h"
 #include "dna.h"
-
-int main(void) {
-	srand((unsigned int)time(NULL));
-	RemoveCursor();
-	DrawBoard();
-	P();
-	ShowScore();
-	DNA::best();
-	while (1) {
-		Reset();
+using namespace std;
+class T {
+public:
+	static void Thread(int parent) {
+		Reset(parent);
+		ShowScore(parent);
 		while (1) {
-			RemoveLine();
-			InitKeyDelayRate(4 + SetSpeed());
-			InitNewBlockPos(4, 0);
-			ChooseBlock();
+			RemoveLine(parent);
+			InitNewBlockPos(4, 0, parent);
+			ChooseBlock(parent);
 			if (!BlockNext()) break;
 			while (1) {
-				if (!BlockDown()) {
+				if (!BlockDown(parent)) {
 					AddBlockInfo();
 					break;
 				}
 				//PLAYER
-				ProcessKeyInput();
 				//AI
-				AI();
+				AI(parent);
 				//TRAINING
-				P();
 			}
+		}
+	}
+};
+int main(void) {
+	srand((unsigned int)time(NULL));
+	int p = GetP();
+	int count = GetC();
+	RemoveCursor();
+	DrawBoard(0);
+	ShowScore(0);
+	thread t[50];
+	DNA _DNA = DNA();
+	T _T = T();
+	char c = _getch();
+	if (c == '0') {
+		_DNA.GenerateNew();
+		_DNA.SaveFile();
+	}
+	if(!_DNA.LoadFile()) _DNA.GenerateNew();
+	_DNA.SaveFile();
+	InitKeyDelayRate(4);
+	while (1) {
+		while (1) {
+			for (int cnt = 0; cnt < count; cnt++) {
+				for (int i = 0; i < p; i++) {
+					t[i] = thread(_T.Thread, i);
+					t[i].join();
+				}
+			}
+			NextGeneration();
+			_DNA.SaveFile();
 		}
 	}
 	return 0;
